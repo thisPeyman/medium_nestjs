694461c9e362765168a90170078f9fcfdbf55053
Peyman <peymankhosravi.honor@gmail.com>
2022-06-16 20:02:12 +0430

    added database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TagModule } from './tag/tag.module';
+import orgconfig from './orm-config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(orgconfig)],
   controllers: [AppController],
   providers: [AppService],
 })
