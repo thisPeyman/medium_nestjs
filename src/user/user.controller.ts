28dfc572a9f09ab047d5a64d8fda9b429d8777c3
Peyman <peymankhosravi.honor@gmail.com>
2022-06-20 13:01:48 +0430

    added login functionality

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,7 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { UserResponseInterface } from './types/user-response.interface';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
@@ -20,6 +21,16 @@ export class UserController {
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
+
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async loginUser(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.loginUser(loginUserDto);
 
     return this.userService.buildUserResponse(user);
   }
